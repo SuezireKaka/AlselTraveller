208b6ed836d2218d644aacb26a67f45dd105b460
SuezireKaka <rwccm3241@naver.com>
2024-07-23 19:24:49 +0900

    sql 기본 데이터 삽입

--- alselTraveller/src/main/resources/sql/data/t_party.sql
+++ alselTraveller/src/main/resources/sql/data/t_party.sql
@@ -1,8 +1,8 @@
 insert into t_party(id, discrim, name)
-select NEXT_PK('s_party') /*'0000'*/ id, 'G' discrim, 'JMemoOper' name
+select NEXT_PK('s_party') /*'0000'*/ id, 'G' discrim, 'Alsel' name;
 
 insert into t_group(id, open, intro)
-values ('0000', 1, '모두 즐겁게 메모합시다ㅡ');
+values ('0000', 1, '아르셰르에 어서오세요');
 
-insert into t_party(id, discrim, selector)
+insert into t_party(id, discrim, name)
 select NEXT_PK('s_party') /*'0001'*/ id, 'U' discrim, 'suezirekaka' name;
